30bd8512ec53db92554f85ddbb4a973100653baf
Vitalina <vitalinabatrakova@gmail.com>
2023-11-02 11:40:25 +0300

    homework

--- src/main/java/ru/geekbrains/lesson3/task2/Freelancer.java
+++ src/main/java/ru/geekbrains/lesson3/task2/Freelancer.java
@@ -1,9 +1,6 @@
 package ru.geekbrains.lesson3.task2;
 
-/**
- * Фрилансер (работник с почасовой оплатой)
- * TODO: Доработать в рамках домашней работы
- */
+
 public class Freelancer extends Employee {
 
     public Freelancer(String surName, String name, double salary) {
